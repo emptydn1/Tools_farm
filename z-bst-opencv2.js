f7eb6f02e4ac9c016f76efb26421ec34e0cac7c9
Your Name <your.email@example.com>
2026-09-10 00:35:07 +0700

    o

--- z-bst-opencv2.js
+++ z-bst-opencv2.js
@@ -699,7 +699,7 @@ const arg = process.argv[2];
         const workerPromises = [];
 
         for (const [index, port] of ports.entries()) {
-            await sleep(500);
+            await sleep(2000);
             const p = runPort(index, port, accounts, { templateImagesPos, templateImagesTodoi, templateImagesCitys, pathMatchforB, path_giao_dich, path_cam_nang });
             workerPromises.push(p);
         }
