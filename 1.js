5856e2e90f510b0ff33bf3cea419d8babaa8e05a
Your Name <your.email@example.com>
2026-09-14 00:04:59 +0700

    o

--- 1.js
+++ 1.js
@@ -223,18 +223,21 @@ async function swipe(host, x1, y1, x2, y2, duration = 300) {
 
     await connectAll();
 
-    let buffer = fs.readFileSync("./1.png")
+    // let buffer = fs.readFileSync("./1.png")
     // let host = "127.0.0.1:16448";
-    // const buffer = await runAdb(["-s", "127.0.0.1:16448", "exec-out", "screencap", "-p"]);
-    const metadata = await sharp(buffer).metadata();
+    const buffer = await runAdb(["-s", "127.0.0.1:16448", "exec-out", "screencap", "-p"]);
+    // const metadata = await sharp(buffer).metadata();
 
     // console.log(metadata.height);
 
-    const pngBuffer = await sharp(buffer)
-        .extract({ left: 480, top: 430, width: 320, height: 70 })
-        .toBuffer();
+    // const pngBuffer = await sharp(buffer)
+    //     .extract({ left: 480, top: 430, width: 320, height: 70 })
+    //     .toBuffer();
 
-    fs.writeFileSync("./xxxxxxxx.png", pngBuffer)
+
+    let i = 1;
+    while (fs.existsSync(`./${i}.png`)) i++;
+    fs.writeFileSync(`./${i}.png`, buffer);
 
 
 
